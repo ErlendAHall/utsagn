58dab90a81dd31f1c1e7765a0396057c4817c012
Erlend Hall <ehall@equinor.com>
2026-01-05 08:56:50 +0100

    housekeeping

--- chroma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHROMA_DB_PORT = exports.CHROMA_DB_HOST = void 0;
-exports.CHROMA_DB_HOST = "chroma.erlendhall.no";
-exports.CHROMA_DB_PORT = 443;
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from db.chroma import UtsagnDBClient
+from typing import Union
+from fastapi import FastAPI
+
+from db.chroma_client import UtsagnDBClient
 
 
-def testing():
-    client = UtsagnDBClient()
-    client.seed_db()
-    return client.peek()
+app = FastAPI()
+utsagn_db_client = UtsagnDBClient()
 
 
-print(testing())
+@app.get("/heartbeat")
+def read_root():
+    return {"Well, hello there"}
+
+
+@app.get("/utsagn/query")
+def read_item(query_text: str):
+    return utsagn_db_client.query_utsagn(query_text)
